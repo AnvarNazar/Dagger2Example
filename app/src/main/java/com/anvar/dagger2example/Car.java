625acd445db09522fc3a3cadebb51b835d167f6b
Anvar Nazar <anvarnasar@ymail.com>
2019-08-01 17:46:32 +0530

    method injection

--- app/src/main/java/com/anvar/dagger2example/Car.java
+++ app/src/main/java/com/anvar/dagger2example/Car.java
@@ -7,16 +7,22 @@ import javax.inject.Inject;
 public class Car {
     private static final String TAG = "Car";
 
-    private Engine engine;
+    @Inject
+    Engine engine;
     private Wheels wheels;
 
     @Inject
-    public Car(Engine engine, Wheels wheels) {
+    public Car(Wheels wheels) {
         this.engine = engine;
         this.wheels = wheels;
     }
 
-    public void drive(){
+    public void drive() {
         Log.d(TAG, "driving...");
+    }
+
+    @Inject
+    public void enableRemote(Remote remote) {
+        remote.setListener(this);
     }
 }
