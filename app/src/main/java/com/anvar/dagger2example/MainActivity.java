74d622b1fc663618e8db8180ae1247d7dbd6c99c
Anvar Nazar <anvarnasar@ymail.com>
2019-08-01 19:37:58 +0530

    injecting values at runtime
    (set modules to component)

--- app/src/main/java/com/anvar/dagger2example/MainActivity.java
+++ app/src/main/java/com/anvar/dagger2example/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.anvar.dagger2example.car.Car;
 import com.anvar.dagger2example.dagger.CarComponent;
 import com.anvar.dagger2example.dagger.DaggerCarComponent;
+import com.anvar.dagger2example.dagger.DieselEngineModule;
 
 import javax.inject.Inject;
 
@@ -20,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CarComponent component = DaggerCarComponent.create();
+        CarComponent component = DaggerCarComponent.builder()
+                .dieselEngineModule(new DieselEngineModule(100))
+                .build();
         component.inject(this);
 
         car.drive();
