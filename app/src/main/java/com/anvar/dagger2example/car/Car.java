ed28d27d90327f7e074190d43e837fcbeb0fb1dc
Anvar Nazar <anvarnasar@ymail.com>
2019-08-02 14:45:36 +0530

    @Singleton singleton implementation in dagger

--- app/src/main/java/com/anvar/dagger2example/car/Car.java
+++ app/src/main/java/com/anvar/dagger2example/car/Car.java
@@ -7,19 +7,20 @@ import javax.inject.Inject;
 public class Car {
     private static final String TAG = "Car";
 
-    @Inject
-    Engine engine;
+    private Driver driver;
+    private Engine engine;
     private Wheels wheels;
 
     @Inject
-    public Car(Wheels wheels) {
+    public Car(Driver driver, Engine engine, Wheels wheels) {
+        this.driver = driver;
         this.engine = engine;
         this.wheels = wheels;
     }
 
     public void drive() {
         engine.start();
-        Log.d(TAG, "driving...");
+        Log.d(TAG, driver + " drives " + this);
     }
 
     @Inject
---
+++ app/src/main/java/com/anvar/dagger2example/car/Driver.java
@@ -0,0 +1,12 @@
+package com.anvar.dagger2example.car;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class Driver {
+
+    @Inject
+    public Driver() {
+    }
+}
